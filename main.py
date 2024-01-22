40ad18d3901e9fb80c736a1fef3fe53b8f9fc395
slowickip <piotr.slowicki@pjwstk.edu.pl>
2024-01-22 21:15:14 +0100

    removed gaussianBayes.py

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import random
 
-from gaussianBayes import naive_bayes_sklearn
 from knn import knn_sklearn
 from naive_bayes import naive_bayes
 
-filename = "letter-recognition-normalized"
+filename = "data/letter-recognition-normalized"
 extension = ".data"
 
 def read_data():
@@ -40,10 +39,10 @@ def normalize_and_save_data(file_name,normalized_filename):
             normalized_file.write(",".join(map(str, normalized_line)) + "\n")
 
 def split_data(lines):
-    file_name = "letter-recognition_trn.data"
+    file_name = "data/letter-recognition_trn.data"
     names = file_name
     file_trn = open(file_name, "w")
-    file_name = "letter-recognition_tst.data"
+    file_name = "data/letter-recognition_tst.data"
     names = names + "," + file_name
     file_tst = open(file_name, "w")
 
@@ -61,16 +60,13 @@ def split_data(lines):
     return names
 
 if __name__ == '__main__':
-    normalize_and_save_data("letter-recognition.data", "letter-recognition-normalized.data")
+    normalize_and_save_data("data/letter-recognition.data", "data/letter-recognition-normalized.data")
     lines = read_data()
 
     names = split_data(lines)
     names_tab = names.split(",")
 
     accuracy = naive_bayes(names_tab[0], names_tab[1])
+    print("Naive bayes: " + str(accuracy))
     accuracy = knn_sklearn(names_tab[0], names_tab[1], 3)
-    accuracy = naive_bayes_sklearn(names_tab[0], names_tab[1])
-    final_string = "Accuracy: " + str(accuracy) + "\n"
-    print("Accuracy: " + str(accuracy))
-
-    write_result(final_string)
+    print("KNN: " + str(accuracy))
